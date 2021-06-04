ddfb42b1b5cad5925afe3fb915cf6d3ab31359d8
dedysutanto <gkiserpongit@gmail.com>
2021-06-04 14:19:24 +0700

    Default to gkiserpong.org

--- shorty/views.py
+++ shorty/views.py
@@ -6,12 +6,16 @@ from shorty.models import Url
 
 class RedirectView(View):
     def get(self, request, short):
-        try:
-            url = Url.objects.get(short_id=short)
-            url.counter += 1
-            url.save()
-            #url = get_object_or_404(Url, short_id=short)
-        except Url.DoesNotExist:
+        if short != None:
+            try:
+                url = Url.objects.get(short_id=short)
+                url.counter += 1
+                url.save()
+                #url = get_object_or_404(Url, short_id=short)
+            except Url.DoesNotExist:
+                url = Url()
+                url.long_url = 'https://gkiserpong.org'
+        else:
             url = Url()
             url.long_url = 'https://gkiserpong.org'
 
