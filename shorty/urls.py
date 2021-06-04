5795e26f3221d581223e37353bee360454532211
dedysutanto <gkiserpongit@gmail.com>
2021-06-04 14:25:31 +0700

    DefaultView

--- shorty/urls.py
+++ shorty/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from shorty.views import RedirectView
+from shorty.views import DefaultView, RedirectView
 
 
 app_name = "shorty"
 
 urlpatterns = [
-    path("<slug:short>/", RedirectView.as_view(), name="index")
+    path("<slug:short>/", RedirectView.as_view(), name="index"),
+    path("", DefaultView.as_view(), name="default"),
     ]
--- shorty/views.py
+++ shorty/views.py
@@ -6,17 +6,18 @@ from shorty.models import Url
 
 class RedirectView(View):
     def get(self, request, short):
-        if short != '':
-            try:
-                url = Url.objects.get(short_id=short)
-                url.counter += 1
-                url.save()
-                #url = get_object_or_404(Url, short_id=short)
-            except Url.DoesNotExist:
-                url = Url()
-                url.long_url = 'https://gkiserpong.org'
-        else:
+        try:
+            url = Url.objects.get(short_id=short)
+            url.counter += 1
+            url.save()
+            #url = get_object_or_404(Url, short_id=short)
+        except Url.DoesNotExist:
             url = Url()
             url.long_url = 'https://gkiserpong.org'
 
         return redirect(url.long_url)
+
+class DefaultView(View):
+    def get(self, request):
+        default_url = "https://gkiserpong.org"
+        return redirect(default_url)
